12f0e121059b516a181dc0acbdccdad98189d7d7
Happy <wugang@billbear.cn>
2019-03-17 22:28:14 +0800

    服务端项目

---
+++ server/src/main/java/org/rxjava/service/mall/annotation/Account.java
@@ -0,0 +1,16 @@
+package org.rxjava.service.mall.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * 是否登陆注解
+ * 默认为登陆
+ *
+ * @author happy 2019-03-17 22:21
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Account {
+    boolean value() default true;
+}
