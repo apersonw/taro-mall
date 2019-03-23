87beaae562045f6074304324e9188b9fc1aed677
Happy <wugang@billbear.cn>
2019-03-23 16:03:52 +0800

    幻灯片效果

--- client/src/components/CustomImage.jsx
+++ client/src/components/CustomImage.jsx
@@ -1,13 +1,19 @@
-import Taro, {Component} from '@tarojs/taro';
-import {Image} from '@tarojs/components';
+import Taro, { Component } from '@tarojs/taro';
+import { Image } from '@tarojs/components';
 
 class CustomImage extends Component {
   render() {
-    let {src, width = 0, height = 0, className, mode = 'scaleToFill', onClick} = this.props;
+    let { src, width = 0, height = 0, className, mode = 'scaleToFill', onClick, style = '' } = this.props;
     width = width && Taro.pxTransform(width);
     height = height && Taro.pxTransform(height);
     return (
-      <Image onClick={onClick} mode={mode} className={className} style={'display: block;width:' + (width || '100%') + ';height:' + (height || '100%') + ';line-height:' + height} src={src} />
+      <Image
+        onClick={onClick}
+        mode={mode}
+        className={className}
+        style={'display: block;width:' + (width || '100%') + ';height:' + (height || '100%') + ';line-height:' + height + ';' + style}
+        src={src}
+      />
     );
   }
 }
--- client/src/pages/index/index.jsx
+++ client/src/pages/index/index.jsx
@@ -4,10 +4,22 @@ import { connect } from '@tarojs/redux';
 import styles from './index.module.scss';
 import action from '../../utils/action';
 import pageWrapper from '../../wrapper/pageWrapper';
+import CustomImage from '../../components/CustomImage';
+import banner1 from '../../assets/banner/1.jpg';
+import banner2 from '../../assets/banner/2.jpg';
+import banner3 from '../../assets/banner/3.jpg';
+import banner4 from '../../assets/banner/4.jpg';
+import banner5 from '../../assets/banner/5.jpg';
+import banner6 from '../../assets/banner/6.jpg';
+import banner7 from '../../assets/banner/7.jpg';
 
 @pageWrapper
 @connect()
 class Index extends Component {
+
+  state = {
+    translateX: 0,
+  };
 
   config = {
     navigationBarTitleText: '首页',
@@ -16,14 +28,37 @@ class Index extends Component {
   componentDidMount() {
     const { dispatch } = this.props;
     dispatch(action('user/fetch'));
+
+    this.timerID = setInterval(
+      this.onTranslateX,
+      2000,
+    );
+  }
+
+  onTranslateX = () => {
+    let { translateX } = this.state;
+    this.setState({ translateX: translateX <= 500 ? translateX + 100 : 0 });
+  };
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
   }
 
   render() {
+    const { translateX } = this.state;
     return (
       <View className={styles.container} >
         <View className='fixedHeader' >导航栏</View >
         <View className='content' >
-          首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页首页
+          <View className={styles.slideList} style={`transform: translateX(-${translateX}%);`} >
+            <CustomImage className={styles.slideImg} style="left:0%;" src={banner1} />
+            <CustomImage className={styles.slideImg} style="left:100%;" src={banner2} />
+            <CustomImage className={styles.slideImg} style="left:200%;" src={banner3} />
+            <CustomImage className={styles.slideImg} style="left:300%;" src={banner4} />
+            <CustomImage className={styles.slideImg} style="left:400%;" src={banner5} />
+            <CustomImage className={styles.slideImg} style="left:500%;" src={banner6} />
+            <CustomImage className={styles.slideImg} style="left:600%;" src={banner7} />
+          </View >
         </View >
         <View className='toolBar' >底部tabBar</View >
       </View >
