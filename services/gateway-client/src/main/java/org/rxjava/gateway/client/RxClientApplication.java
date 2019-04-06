8037454058af1ea85142802ca24d9a3aa6abaa63
Happy <wugang@billbear.cn>
2019-04-06 17:12:31 +0800

    修改设计稿和网关

--- services/gateway-client/src/main/java/org/rxjava/gateway/client/RxClientApplication.java
+++ services/gateway-client/src/main/java/org/rxjava/gateway/client/RxClientApplication.java
@@ -3,6 +3,9 @@ package org.rxjava.gateway.client;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -10,8 +13,8 @@ import reactor.core.publisher.Mono;
 /**
  * @author happy 2019-03-17 22:10
  */
-@SpringBootApplication
 @RestController
+@SpringBootApplication
 public class RxClientApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(RxClientApplication.class).web(WebApplicationType.REACTIVE).run(args);
@@ -20,5 +23,15 @@ public class RxClientApplication {
     @GetMapping("/")
     public Mono<String> system() {
         return Mono.just("客户端网关正常");
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/user/**")
+                        .uri("http://localhost:8081")
+                        .id("user_service")
+                )
+                .build();
     }
 }
---
+++ services/gateway-client/src/main/java/org/rxjava/gateway/client/controller/ProxyController.java
@@ -0,0 +1,12 @@
+package org.rxjava.gateway.client.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author happy 2019-04-06 07:26
+ */
+@RestController
+@RequestMapping("user")
+public class ProxyController {
+}
