29d663af99dc009f02e299c732e5b4e680ce4525
Happy <wugang@billbear.cn>
2019-04-28 00:05:12 +0800

    配置时间解析

--- services/rxjava-common-parent/rxjava-service-starter/src/main/java/org/rxjava/service/starter/RxJavaServiceAutoConfiguration.java
+++ services/rxjava-common-parent/rxjava-service-starter/src/main/java/org/rxjava/service/starter/RxJavaServiceAutoConfiguration.java
@@ -1,6 +1,18 @@
 package org.rxjava.service.starter;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.rxjava.common.core.utils.JsonUtils;
 import org.rxjava.service.starter.boot.RxjavaDelegatingWebFluxConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
@@ -19,6 +31,11 @@ import org.springframework.http.codec.json.Jackson2JsonDecoder;
 import org.springframework.http.codec.json.Jackson2JsonEncoder;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
 /**
  * @author happy 2019-04-09 01:32
  * 响应式服务自动配置starter
@@ -29,15 +46,60 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
 @EnableMongoAuditing
 @AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE)
 public class RxJavaServiceAutoConfiguration implements WebFluxConfigurer {
+    private static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss.SSS";
+    private static final String DATE_FORMAT = "yyyy-MM-dd";
+    private static final String TIME_FORMAT = "HH:mm:ss.SSS";
+
     @Bean
     @Primary
     ReactiveRedisTemplate<String, String> reactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
         return new ReactiveRedisTemplate<>(factory, RedisSerializationContext.string());
     }
 
+    /**
+     * 配置时间格式解析
+     */
     @Bean
     public ObjectMapper objectMapper() {
-        return JsonUtils.create();
+        ObjectMapper objectMapper = JsonUtils.create();
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objectMapper.registerModule(new ParameterNamesModule())
+                .registerModule(new Jdk8Module());
+
+        DateTimeFormatter dataTimeFormatter = DateTimeFormatter.ofPattern(DATE_TIME_FORMAT).withZone(ZoneId.systemDefault());
+        DateTimeFormatter dataFormatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern(TIME_FORMAT);
+
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(dataFormatter));
+        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(dataFormatter));
+
+        javaTimeModule.addDeserializer(LocalTime.class, new LocalTimeDeserializer(timeFormatter));
+        javaTimeModule.addSerializer(LocalTime.class, new LocalTimeSerializer(timeFormatter));
+
+        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(dataTimeFormatter));
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(dataTimeFormatter));
+
+        javaTimeModule.addDeserializer(Instant.class, new JsonDeserializer<Instant>() {
+            @Override
+            public Instant deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+                return Instant.from(dataTimeFormatter.parse(jsonParser.getText()));
+            }
+        });
+
+        javaTimeModule.addSerializer(Instant.class, new JsonSerializer<Instant>() {
+            @Override
+            public void serialize(Instant value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+                String str = dataTimeFormatter.format(value);
+                gen.writeString(str);
+            }
+        });
+
+        objectMapper.registerModule(javaTimeModule);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(DATE_TIME_FORMAT);
+        simpleDateFormat.setLenient(false);
+        objectMapper.setDateFormat(simpleDateFormat);
+        return objectMapper;
     }
 
     @Bean
