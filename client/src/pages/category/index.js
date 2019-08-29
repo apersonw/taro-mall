b662e831cb81bb5ce9c7a09636c54c4d3b538e74
Happy <wugang@billbear.cn>
2019-08-30 02:35:39 +0800

    更新分类mock数据，新增gitlab的ci

--- client/src/pages/category/index.js
+++ client/src/pages/category/index.js
@@ -1,6 +1,7 @@
 import Taro, { Component } from '@tarojs/taro';
 import { View, ScrollView } from '@tarojs/components';
 import { connect } from '@tarojs/redux';
+import menuList from '@/mock/category/menu.json';
 import styles from './index.module.scss';
 import TabBar from '../../components/TabBar';
 
@@ -15,21 +16,7 @@ class Index extends Component {
     return (
       <View className={styles.container} >
         <ScrollView scrollY className={styles.firstCategory} >
-          <View className={styles.categoryBox} >热门推荐</View >
-          <View className={styles.categoryBox} >手机数码</View >
-          <View className={styles.categoryBox} >家用电器</View >
-          <View className={styles.categoryBox} >电脑办公</View >
-          <View className={styles.categoryBox} >计生情趣</View >
-          <View className={styles.categoryBox} >美妆护肤</View >
-          <View className={styles.categoryBox} >个护清洁</View >
-          <View className={styles.categoryBox} >汽车生活</View >
-          <View className={styles.categoryBox} >京东超市</View >
-          <View className={styles.categoryBox} >男装</View >
-          <View className={styles.categoryBox} >男鞋</View >
-          <View className={styles.categoryBox} >女装</View >
-          <View className={styles.categoryBox} >女鞋</View >
-          <View className={styles.categoryBox} >男婴童装</View >
-          <View className={styles.categoryBox} >图书音像</View >
+          {menuList.map((menu, index) => <View key={index} className={styles.categoryBox} >{menu}</View >)}
         </ScrollView >
         <ScrollView >
           <View >手机</View >
